f3b3f55a17c9648c60218cd5ab31d8e1f38aa801
davide <davidetacchini11@gmail.com>
2020-10-20 23:57:37 +0200

    replaced __str__ with super() in custom exceptions

--- utils/data.py
+++ utils/data.py
@@ -12,33 +12,35 @@ class RequestError(Exception):
 class NotFound(RequestError):
     """Exception raised when a profile is not found."""
 
-    def __str__(self):
-        return (
+    def __init__(self):
+        super().__init__(
             "Player not found. Please make sure you aren't missing any capital letter."
         )
 
 
 class BadRequest(RequestError):
-    """Exception raised when a request sucks.
+    """Exception raised when a request sucks."""
 
-    E.g: Battletag has no tag."""
-
-    def __str__(self):
-        return "Wrong battletag format entered! Correct format: `name#0000`"
+    def __init__(self):
+        super().__init__("Wrong battletag format entered! Correct format: `name#0000`")
 
 
 class InternalServerError(RequestError):
     """Exception raised when the API returns 500 status code."""
 
-    def __str__(self):
-        return "The API is having internal server problems. Please be patiente and try again later."
+    def __init__(self):
+        super().__init__(
+            "The API is having internal server problems. Please be patiente and try again later."
+        )
 
 
 class ServiceUnavailable(RequestError):
     """Exception raised when the server API is under maintenance."""
 
-    def __str__(self):
-        return "The API is under maintenance. Please be patiente and try again later."
+    def __init__(self):
+        super().__init__(
+            "The API is under maintenance. Please be patiente and try again later."
+        )
 
 
 class Data:
