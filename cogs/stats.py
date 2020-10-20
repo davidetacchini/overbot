4aad7d49edc12206499b90cea179cff205032310
davide <davidetacchini11@gmail.com>
2020-10-20 17:44:26 +0200

    Using data object to get data

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,7 +1,8 @@
 from discord.ext import commands
 
+from utils.data import RequestError
+from utils.player import Player, NoStatistics, NoHeroStatistics
 from utils.globals import embed_exception
-from utils.profile import Profile, NoStatistics, NoHeroStatistics
 from classes.converters import Hero, Platform, Username
 
 
@@ -9,68 +10,45 @@ class Statistics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @staticmethod
-    async def embed_stats(ctx, platform, name, hero=None):
-        """Returns players formatted stats."""
-        url = f"{ctx.bot.config.base_url}/{platform}/{name}/complete"
-        command = str(ctx.command.name).lower()
-
-        async with ctx.bot.session.get(url) as r:
-            if r.status == 200:
-                data = await r.json()
-            elif r.status == 400:
-                return await ctx.send(
-                    f"Wrong battletag format! Correct format: {name}#0000"
-                )
-            elif r.status == 404:
-                return await ctx.send(
-                    "Profile not found. Please make sure you aren't missing any capital letter."
-                )
-            elif r.status == 500:
-                return await ctx.send(
-                    "API Internal server error. Please be patiente and try again."
-                )
-            else:
-                return await ctx.send(
-                    "The API is under maintenance. Please be patiente and try again later."
-                )
-
-        profile = Profile(data=data, platform=platform, name=name)
-
-        try:
-            if profile.is_private:
-                embed = profile.private(ctx)
-            elif command == "rank":
-                embed = profile.rank()
-            elif command == "hero":
-                embed = profile.hero(ctx, hero)
-            else:
-                embed = profile.statistics(ctx)
-            await ctx.bot.paginator.Paginator(extras=embed).paginate(ctx)
-        except NoStatistics:
-            await ctx.send(
-                "This profile has no quick play nor competitive statistics to display."
-            )
-        except NoHeroStatistics:
-            await ctx.send(
-                f"This profile has no quick play nor competitive stats for **{hero}** to display."
-            )
-        except Exception as exc:
-            await ctx.send(embed=embed_exception(exc))
-
     @commands.command(aliases=["rating"])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def rank(self, ctx, platform: Platform, *, username: Username):
         """Returns player ranks."""
         async with ctx.typing():
-            await self.embed_stats(ctx, platform, username)
+            try:
+                data = await self.bot.data.Data(platform=platform, name=username).get()
+            except RequestError as exc:
+                await ctx.send(exc)
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
+            profile = Player(data=data, platform=platform, name=username).rank()
+            try:
+                await ctx.send(embed=profile)
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def stats(self, ctx, platform: Platform, *, username: Username):
         """Returns player both competitive and quick play statistics."""
         async with ctx.typing():
-            await self.embed_stats(ctx, platform, username)
+            try:
+                data = await self.bot.data.Data(platform=platform, name=username).get()
+            except RequestError as exc:
+                await ctx.send(exc)
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
+            profile = Player(data=data, platform=platform, name=username).statistics(
+                ctx
+            )
+            try:
+                await self.bot.paginator.Paginator(extras=profile).paginate(ctx)
+            except NoStatistics:
+                await ctx.send(
+                    "This profile has no quick play nor competitive statistics to display."
+                )
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
 
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -84,7 +62,23 @@ class Statistics(commands.Cog):
     ):
         """Returns player stats for a given hero."""
         async with ctx.typing():
-            await self.embed_stats(ctx, platform, username, hero)
+            try:
+                data = await self.bot.data.Data(platform=platform, name=username).get()
+            except RequestError as exc:
+                await ctx.send(exc)
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
+            profile = Player(data=data, platform=platform, name=username).hero(
+                ctx, hero
+            )
+            try:
+                await self.bot.paginator.Paginator(extras=profile).paginate(ctx)
+            except NoHeroStatistics:
+                await ctx.send(
+                    f"This profile has no quick play nor competitive stats for **{hero}** to display."
+                )
+            except Exception as exc:
+                await ctx.send(embed=embed_exception(exc))
 
 
 def setup(bot):
