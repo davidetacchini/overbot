4f57af0ceddf43135164b2c7efbbece364f92ff0
davide <davidetacchini11@gmail.com>
2020-10-25 22:47:00 +0100

    Moved all the tasks to a new file tasks.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,17 +1,13 @@
-import re
 import time
 import textwrap
-from random import randint
 
 import discord
-from bs4 import BeautifulSoup
-from discord.ext import tasks, commands
+from discord.ext import commands
 
 
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.send_news.start()
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -60,62 +56,6 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         await self.bot.pool.execute("DELETE FROM server WHERE id=$1;", guild.id)
-
-    @tasks.loop(minutes=5.0)
-    async def send_news(self):
-        await self.bot.wait_until_ready()
-        async with self.bot.session.get(self.bot.config.overwatch["news"]) as r:
-            content = await r.read()
-        page = BeautifulSoup(content, features="html.parser")
-        news = page.find("section", {"class": "NewsHeader-featured"}).findChild()
-        # get the news ID from the URL
-        news_id = re.search(r"\d+", news["href"]).group(0)
-
-        if int(news_id) == await self.bot.pool.fetchval(
-            "SELECT news_id FROM news WHERE id=1;"
-        ):
-            return
-
-        title = news.find("h1", {"class": "Card-title"})
-        img = news.find("div", {"class", "Card-thumbnail"})
-        img_url = img["style"].split("url(")[1][:-1]
-
-        embed = discord.Embed(
-            title=title.get_text(),
-            url="https://playoverwatch.com" + news["href"],
-            color=self.bot.color,
-            timestamp=self.bot.timestamp,
-        )
-
-        embed.set_image(url=f"https:{img_url}")
-        embed.set_footer(text="Blizzard Entertainment")
-        if randint(0, 2) == 1:
-            embed.add_field(
-                name="Info",
-                value="You can disable news feed notification by running the `settings news disable`.",
-            )
-
-        channels = await self.bot.pool.fetch(
-            "SELECT news_channel FROM server WHERE news_channel <> 0;"
-        )
-
-        for channel in channels:
-            c = self.bot.get_channel(channel["news_channel"])
-            try:
-                await c.send(embed=embed)
-            except AttributeError:
-                # if the channel set for the news notification has been deleted, reset to 0
-                await self.bot.pool.execute(
-                    "UPDATE server SET news_channel=0 WHERE news_channel=$1;",
-                    channel["news_channel"],
-                )
-
-        await self.bot.pool.execute(
-            "UPDATE news SET news_id=$1 WHERE id=1;", int(news_id)
-        )
-
-    def cog_unload(self):
-        self.send_news.cancel()
 
 
 def setup(bot):
--- cogs/patrons.py
+++ cogs/patrons.py
@@ -1,14 +1,11 @@
-import discord
-from discord.ext import tasks, commands
+from discord.ext import commands
 
 from utils.checks import is_donator, has_profile
-from utils.player import Player
 
 
 class Patron(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.track_profile.start()
 
     @is_donator()
     @has_profile()
@@ -37,34 +34,6 @@ class Patron(commands.Cog):
             "UPDATE profile SET track=false WHERE id=$1", ctx.author.id
         )
         await ctx.send("Your profile will no longer be monitored.")
-
-    @tasks.loop(hours=24.0)
-    async def track_profile(self):
-        await self.bot.wait_until_ready()
-        if self.bot.is_ready():
-            # don't send the message everytime the bot starts
-            return
-
-        profiles = await self.bot.pool.fetch(
-            "SELECT id, platform, name FROM profile WHERE track <> false"
-        )
-        for profile in profiles:
-            user = self.bot.get_user(profile["id"])
-            data = await self.bot.data.Data(
-                platform=profile["platform"], name=profile["name"]
-            ).get()
-
-            try:
-                await user.send(
-                    embed=Player(
-                        data=data, platform=profile["platform"], name=profile["name"]
-                    ).rank()
-                )
-            except discord.Forbidden:
-                return
-
-    def cog_unload(self):
-        self.track_profile.cancel()
 
 
 def setup(bot):
