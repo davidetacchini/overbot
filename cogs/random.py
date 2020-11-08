11ec98f4dfe510601effc63570f4d3ef807dbbd6
Davide <84925446+davidetacchini@users.noreply.github.com>
2020-11-08 19:47:06 +0100

    Added the possibility to pass a category when using random hero command

--- cogs/random.py
+++ cogs/random.py
@@ -2,6 +2,7 @@ import secrets
 
 import discord
 from discord.ext import commands
+from classes.converters import Category
 
 ROLES = [
     {
@@ -34,7 +35,7 @@ class Random(commands.Cog):
     async def get_hero(self):
         url = "https://overwatch-api.tekrop.fr/heroes"
         async with self.bot.session.get(url) as r:
-            return secrets.choice(await r.json())
+            return await r.json()
 
     @staticmethod
     def get_hero_color(hero):
@@ -44,8 +45,17 @@ class Random(commands.Cog):
             return 0xE61B23
         return 0x13A549
 
-    async def random_hero(self):
-        hero = await self.get_hero()
+    def get_category_heroes(self, heroes, category):
+        return [hero for hero in heroes if hero["role"] == category]
+
+    async def random_hero(self, category):
+        heroes = await self.get_hero()
+
+        if not category:
+            hero = secrets.choice(heroes)
+        else:
+            category_heroes = self.get_category_heroes(heroes, category)
+            hero = secrets.choice(category_heroes)
 
         embed = discord.Embed(color=self.get_hero_color(hero))
         embed.title = hero["name"].upper()
@@ -73,18 +83,23 @@ class Random(commands.Cog):
         """Returns a random hero or role, based on your choice.
 
         Choices
-            - Hero: random hero
-            - Role: random role
+        - Hero: random hero
+        - Role: random role
         """
         embed = self.bot.get_subcommands(ctx, self.bot.get_command(ctx.command.name))
         await ctx.send(embed=embed)
 
-    @random.command()
+    @random.command(invoke_without_command=True)
     @commands.cooldown(1, 3, commands.BucketType.user)
-    async def hero(self, ctx):
-        """Returns a random hero to play."""
+    async def hero(self, ctx, category: Category = None):
+        """Returns a random hero to play.
+
+        `[category]` - The category to get a random hero from.
+
+        If no category is passed, a random hero will be choose from all the categories.
+        """
         try:
-            embed = await self.random_hero()
+            embed = await self.random_hero(category)
         except Exception as exc:
             await ctx.send(embed=self.bot.embed_exception(exc))
         else:
