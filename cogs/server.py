4dae6bda2e8643d33ac6ce27a48988a3af430ebb
davide <davidetacchini11@gmail.com>
2020-11-29 16:21:23 +0100

    Move -leaderboard in Server cog

--- cogs/server.py
+++ cogs/server.py
@@ -49,6 +49,47 @@ class Server(commands.Cog):
             )
             await ctx.send(embed=embed)
 
+    @staticmethod
+    def get_placement(place):
+        placements = {
+            1: "<:top500:632281138832080926>",
+            2: "<:grandmaster:632281128966946826>",
+            3: "<:master:632281117394993163>",
+            4: "<:diamond:632281105571119105>",
+            5: "<:platinum:632281092875091998>",
+        }
+        return placements.get(place)
+
+    @commands.command()
+    @commands.cooldown(1, 30.0, commands.BucketType.user)
+    async def leaderboard(self, ctx):
+        """Displays a leaderboard of the 5 most active servers.
+
+        It is based on commands runned.
+        """
+        async with ctx.typing():
+            guilds = await self.bot.pool.fetch(
+                "SELECT id, commands_runned FROM server WHERE id NOT IN"
+                " (638339745117896745, 550685823784321035) ORDER BY commands_runned DESC LIMIT 5;"
+            )
+            embed = discord.Embed()
+            embed.title = "Five Most Active Servers"
+            embed.url = self.bot.config.website
+            embed.set_footer(text="Tracking command usage since • 11/26/2020")
+
+            board = ""
+            for i, guild in enumerate(guilds, start=1):
+                g = self.bot.get_guild(guild["id"])
+                board += (
+                    f"{self.get_placement(i)} **{str(g)}**"
+                    f" runned a total of **{guild['commands_runned']}** commands\n"
+                    f"Joined on: **{str(g.me.joined_at).split(' ')[0]}**\n"
+                )
+                if i < 5:
+                    board += "-----------\n"
+            embed.description = board
+            await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Server(bot))
