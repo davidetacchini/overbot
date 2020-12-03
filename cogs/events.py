146a4f7bbe9959ae1c5ad3436407eaadc7af5fda
Davide <84925446+davidetacchini@users.noreply.github.com>
2020-12-03 18:29:39 +0100

    Add all bot events; caching all heroes at startup

--- cogs/events.py
+++ cogs/events.py
@@ -4,14 +4,30 @@ from contextlib import suppress
 
 import discord
 from discord.ext import commands
+import os
 
 
 class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def send_log(self, color, message):
+        if self.bot.debug:
+            return
+
+        embed = discord.Embed(color=color)
+        embed.title = message
+
+        channel = self.bot.get_channel(self.bot.config.status_channel)
+
+        if not channel:
+            return
+
+        await channel.send(embed=embed)
+
     @commands.Cog.listener()
     async def on_ready(self):
+        await self.send_log(discord.Color.blue(), "Bot is online.")
         print(
             textwrap.dedent(
                 f"""
@@ -24,23 +40,40 @@ class Events(commands.Cog):
             """
             )
         )
+
         if not hasattr(self.bot, "uptime"):
             self.bot.uptime = datetime.utcnow()
+
+        if not hasattr(self.bot, "total_lines"):
+            self.bot.total_lines = 0
+            self.get_line_count()
+
+        # caching all Overwatch heroes at startup. Used in
+        # classes/converters.py to check if the entered hero exists.
+        if not hasattr(self.bot, "heroes"):
+            self.bot.heroes = await self.cache_heroes()
+
         await self.change_presence()
 
     @commands.Cog.listener()
-    async def on_resumed(self):
-        print("Connection resumed.")
+    async def on_disconnect(self):
+        message = "Connection lost."
+        await self.send_log(discord.Color.red(), message)
 
-    async def change_presence(self):
-        await self.bot.wait_until_ready()
-        await self.bot.change_presence(
-            activity=discord.Activity(
-                name=f"{self.bot.prefix}help",
-                type=discord.ActivityType.playing,
-            ),
-            status=discord.Status.idle,
-        )
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        message = "Connection resumed"
+        await self.send_log(discord.Color.green(), message)
+
+    @commands.Cog.listener()
+    async def on_shard_disconnect(self, shard_id):
+        message = f"Shard {shard_id + 1} disconnected."
+        await self.send_log(discord.Color.red(), message)
+
+    @commands.Cog.listener()
+    async def on_shard_connect(self, shard_id):
+        message = f"Shard {shard_id + 1} connected."
+        await self.send_log(discord.Color.green(), message)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -55,6 +88,30 @@ class Events(commands.Cog):
         with suppress(KeyError):
             del self.bot.prefixes[guild.id]
         await self.bot.pool.execute("DELETE FROM server WHERE id = $1;", guild.id)
+
+    async def change_presence(self):
+        await self.bot.wait_until_ready()
+        await self.bot.change_presence(
+            activity=discord.Activity(
+                name=f"{self.bot.prefix}help",
+                type=discord.ActivityType.playing,
+            ),
+            status=discord.Status.idle,
+        )
+
+    def get_line_count(self):
+        for root, dirs, files in os.walk(os.getcwd()):
+            [dirs.remove(d) for d in list(dirs) if d == "env"]
+            for name in files:
+                if name.endswith(".py"):
+                    with open(f"{root}/{name}") as f:
+                        self.bot.total_lines += len(f.readlines())
+
+    async def cache_heroes(self):
+        url = self.bot.config.random["hero"]
+        async with self.bot.session.get(url) as r:
+            heroes = await r.json()
+        return [str(h["key"]).lower() for h in heroes]
 
 
 def setup(bot):
