2e95eaef46e32dd79c2dc7328a49c85d25799ec3
Davide <84925446+davidetacchini@users.noreply.github.com>
2020-12-17 22:53:22 +0100

    Using better names for variables

--- utils/data.py
+++ utils/data.py
@@ -44,56 +44,56 @@ class ServiceUnavailable(RequestError):
 class TooManyAccounts(RequestError):
     """Exception raised when the API found too many accounts under that name."""
 
-    def __init__(self, platform, name, names):
+    def __init__(self, platform, username, players):
         if platform == "pc":
             message = (
-                f"**{len(names)}** accounts found under the name of `{name}`"
+                f"**{players}** accounts found under the name of `{username}`"
                 f" playing on `{platform}`. Please be more specific by entering"
-                f" your full battletag in the following format: `name#0000`"
+                f" the full battletag in the following format: `name#0000`"
             )
         else:
             message = (
-                f"**{len(names)}** accounts found under the name of `{name}`"
+                f"**{players}** accounts found under the name of `{username}`"
                 f" playing on `{platform}`. Please be more specific."
             )
         super().__init__(message)
 
 
 class Data:
-    __slots__ = ("platform", "name")
+    __slots__ = ("platform", "username")
 
-    def __init__(self, **kwargs):
-        self.platform = kwargs.get("platform", None)
-        self.name = kwargs.get("name", None)
+    def __init__(self, *, platform: str, username: str):
+        self.platform = platform
+        self.username = username
 
     @property
     def account_url(self):
-        return config.overwatch["account"] + "/" + self.name + "/"
+        return config.overwatch["account"] + "/" + self.username + "/"
 
-    async def resolve_name(self, names):
-        if len(names) == 1:
-            return names[0]["urlName"]
-        elif len(names) > 1:
-            total_names = []
-            for name in names:
+    async def resolve_name(self, players):
+        if len(players) == 1:
+            return players[0]["urlName"]
+        elif len(players) > 1:
+            total_players = []
+            for player in players:
                 if (
-                    name["name"].lower() == self.name.lower()
-                    and name["platform"] == self.platform
+                    player["name"].lower() == self.username.lower()
+                    and player["platform"] == self.platform
                 ):
-                    return name["urlName"]
-                if name["platform"] == self.platform:
-                    total_names.append(name["name"].lower())
+                    return player["urlName"]
+                if player["platform"] == self.platform:
+                    total_players.append(player["name"].lower())
             if (
-                len(total_names) == 0
-                or "#" in self.name
-                and self.name.lower() not in total_names
+                len(total_players) == 0
+                or "#" in self.username
+                and self.username.lower() not in total_players
             ):
                 raise NotFound()
             else:
-                raise TooManyAccounts(self.platform, self.name, total_names)
+                raise TooManyAccounts(self.platform, self.username, len(total_players))
         else:
-            # return self.name and let resolve_response handle it
-            return self.name
+            # return the username and let `resolve_response` handle it
+            return self.username
 
     async def get_name(self):
         async with aiohttp.ClientSession() as s:
