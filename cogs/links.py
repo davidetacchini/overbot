814e0a94c105a64adaddfff4fee044d0237375d6
Davide <84925446+davidetacchini@users.noreply.github.com>
2020-12-21 18:09:56 +0100

    Update '-donate' command; aliased to 'paypal' and 'tip'

--- cogs/links.py
+++ cogs/links.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 
 class Links(commands.Cog):
@@ -20,10 +21,22 @@ class Links(commands.Cog):
         """Returns bot invite link."""
         await ctx.send(self.bot.config.invite)
 
-    @commands.command()
+    @commands.command(aliases=["paypal", "tip"])
     async def donate(self, ctx):
         """Returns the developer's PayPal."""
-        await ctx.send(self.bot.config.paypal)
+        embed = discord.Embed(color=discord.Color.blue())
+        embed.title = "Support OverBot"
+        embed.url = self.bot.config.paypal
+        guild = await self.bot.fetch_guild(self.bot.config.support_server_id)
+        owner = await guild.fetch_member(self.bot.config.owner_id)
+        embed.description = (
+            "Maintaining OverBot and adding new features takes up a huge "
+            f'portion of [{str(owner)}]({self.bot.config.github["profile"]})\'s spare time. '
+            f"If you enjoy using OverBot you can donate a small amount "
+            "to keep the project alive or just support me. **Thank you!**\n\n"
+            f"{self.bot.config.paypal}"
+        )
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=["git"])
     async def github(self, ctx):
