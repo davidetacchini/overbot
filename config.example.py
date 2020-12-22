c2648666328f762e48d8b175c7f4c9d895906b15
Davide <84925446+davidetacchini@users.noreply.github.com>
2020-12-23 00:05:02 +0100

    Add configuration example

---
+++ config.example.py
@@ -0,0 +1,134 @@
+"""Whether the version is realase or beta."""
+DEBUG = True
+
+"""The Bot token."""
+token = "your_bot_token"
+
+"""Database credentials."""
+database = {
+    "user": "username",
+    "password": "password",
+    "database": "database",
+    "host": "hostname",
+}
+
+# IGNORE (you don't actually need them to run the bot)
+# NOTE: DEBUG must be set to True though
+
+"""Webhook credentials."""
+webhook = {
+    "id": "your webhook id (integer)",
+    "token": "webhook_token",
+}
+
+"""OverBot private API used to share bot information to its website."""
+obapi = {
+    "url": "secret",
+    "token": "token",
+}
+
+"""Discord Bot List (top.gg) authorization."""
+top_gg = {
+    "url": "top.gg url",
+    "token": "top.gg_token",
+}
+
+"""Discord Bot List (discordbotlist.gg) authorization."""
+dbl = {
+    "url": "dbl url",
+    "token": "dbl_token",
+}
+
+"""discord.bots.gg authorization."""
+discord_bots = {
+    "url": "discord_bots url",
+    "token": "discord_bots_token",
+}
+
+# ENDIGNORE
+
+"""Loading GIF"""
+loading_gif = "https://i.imgur.com/DwjPYQn.gif"
+
+"""The owner's ID."""
+owner_id = 285502621295312896
+
+"""The version of the Bot."""
+version = "3.2.0"
+
+"""Prefix used if custom is not set."""
+default_prefix = "-"
+
+"""Default color used on most of the embeds"""
+main_color = 0xFA9C1D
+
+"""Channel ID for feedback logs."""
+feedback_channel = 581888563256229891
+
+"""Channel ID for error logs."""
+errors_channel = 649188058574356481
+
+"""OverBot's server ID."""
+support_server_id = 550685823784321035
+
+"""Overwatch news feed channel."""
+news_channel = 771489241288540220
+
+"""Bot status channel."""
+status_channel = 784091607540039790
+
+"""Channel ID for trivia submits."""
+trivia_channel = 783149071217065985
+
+"""Hero portrait URL."""
+hero_url = "https://d1u1mce87gyfbn.cloudfront.net/hero/{}/hero-select-portrait.png"
+
+"""Overwatch API url (unofficial)."""
+base_url = "https://ow-api.com/v3/stats"
+
+"""GitHub links."""
+github = {
+    "profile": "https://github.com/davidetacchini/",
+    "repo": "https://github.com/davidetacchini/overbot",
+}
+
+"""Random endpoints."""
+random = {
+    "hero": "https://overwatch-api.tekrop.fr/heroes",
+    "map": "https://api.overwatchleague.com/maps",
+}
+
+"""Overwatch endpoints."""
+overwatch = {
+    "status": "https://downdetector.com/status/overwatch/",
+    "patch": "https://playoverwatch.com/en-us/news/patch-notes/{}",
+    "news": "https://playoverwatch.com/en-us/news/",
+    "player": "https://playoverwatch.com/en-us/career/{}/{}/",
+    "account": "https://playoverwatch.com/en-us/search/account-by-name",
+}
+
+"""Python logo."""
+python_logo = "https://i.imgur.com/6pg6Xv4.png"
+
+"""Official website."""
+website = "https://overbot.netlify.app"
+
+"""Official support server invite."""
+support = "https://discord.gg/8g3jnxv"
+
+"""Invite link."""
+invite = "https://discord.com/api/oauth2/authorize?client_id=547546531666984961&permissions=134498368&scope=bot"
+
+"""Vote link."""
+vote = "https://top.gg/bot/547546531666984961/vote"
+
+"""PayPal URL."""
+paypal = "https://www.paypal.com/paypalme/DavideTacchini"
+
+"""Guilds to ignore when getting data from DB."""
+ignored_guilds = (
+    550685823784321035,
+    638339745117896745,
+    264445053596991498,
+    110373943822540800,
+)
