21417897c02249dc5eb258e92d16c8112af68587
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-01-02 14:24:49 +0100

    Add total nicknames in -admin

--- cogs/owner.py
+++ cogs/owner.py
@@ -268,6 +268,7 @@ class Owner(commands.Cog):
             prefixes = self.bot.prefixes
             guilds = await self.bot.pool.fetchval("SELECT COUNT(*) FROM server;")
             ratings = await self.bot.pool.fetchval("SELECT COUNT(*) FROM rating;")
+            nicknames = await self.bot.pool.fetchval("SELECT COUNT(*) FROM nickname")
 
             total_commands = await self.bot.total_commands()
             played, won, lost, contribs = await self.bot.pool.fetchrow(
@@ -278,6 +279,7 @@ class Owner(commands.Cog):
                 ("Total profiles linked", profiles),
                 ("Total prefixes set", len(prefixes)),
                 ("Total profile ratings", ratings),
+                ("Total nicknames", nicknames),
                 ("Total guilds", guilds),
                 ("Total commands runned", total_commands),
             )
