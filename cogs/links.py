009e260880ee4c90dc78a3722c792557e78bac97
davide <davidetacchini11@gmail.com>
2021-03-25 20:54:58 +0100

    Remove donate command

--- cogs/links.py
+++ cogs/links.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 
 from utils.i18n import _, locale
@@ -25,28 +24,6 @@ class Links(commands.Cog):
     async def invite(self, ctx):
         _("""Returns bot invite link.""")
         await ctx.send(self.bot.config.invite)
-
-    @commands.command(aliases=["kofi", "tip"])
-    @locale
-    async def donate(self, ctx):
-        _("""Returns OverBot's ko-fi page.""")
-        embed = discord.Embed(color=discord.Color.blue())
-        embed.title = _("Support OverBot")
-        embed.url = self.bot.config.kofi
-        guild = await self.bot.fetch_guild(self.bot.config.support_server_id)
-        owner = await guild.fetch_member(self.bot.config.owner_id)
-        embed.description = _(
-            "Maintaining OverBot and adding new features takes up a huge "
-            "portion of [{owner}]({github_profile})'s spare time. "
-            "If you enjoy using OverBot you can donate a small amount "
-            "to keep the project alive or just support me. **Thank you!**\n\n"
-            "{kofi}"
-        ).format(
-            owner=str(owner),
-            github_profile=self.bot.config.github["profile"],
-            kofi=self.bot.config.kofi,
-        )
-        await ctx.send(embed=embed)
 
     @commands.command(aliases=["git"])
     @locale
