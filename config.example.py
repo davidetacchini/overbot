eb4df445045964e8ed454ea73352fbd8b408ed4e
davide <davidetacchini11@gmail.com>
2021-03-27 21:08:20 +0100

    Add DonateBot information

--- config.example.py
+++ config.example.py
@@ -15,6 +15,17 @@ database = {
 # IGNORE (you don't actually need them to run the bot)
 # NOTE: DEBUG must be set to True though
 
+"""DonateBot API authorization."""
+dbot = {
+    "new": "endpoint for new donations",
+    "mark": "endpoint to mark donation as processed",
+    "api_key": "personal api key",
+    "product_ids": {
+        "member": "member product ID",
+        "server": "server product ID"
+    },
+}
+
 """Webhook credentials."""
 webhook = {
     "id": 0,
