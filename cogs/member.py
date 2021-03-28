74bd3f5c0920936e41d160ed8fc028847f3bb557
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-03-28 22:31:38 +0200

    Add member settings; refactored '-settings color'

--- cogs/member.py
+++ cogs/member.py
@@ -14,7 +14,7 @@ class Member(commands.Cog):
     @locale
     async def premium(self, ctx):
         _("""Shows your current premium status.""")
-        embed = discord.Embed(color=self.bot.get_color(ctx.author.id))
+        embed = discord.Embed(color=self.bot.color(ctx.author.id))
         embed.title = _("Premium Status")
 
         one_premium = False
@@ -44,12 +44,50 @@ class Member(commands.Cog):
         embed.description = description
         await ctx.send(embed=embed)
 
+    async def get_member_settings(self, member_id):
+        color = self.bot.color(member_id)
+        color_value = str(hex(color)).replace("0x", "#")
+
+        return {
+            "color": color_value,
+        }
+
+    async def embed_member_settings(self, ctx, command):
+        subcommands = getattr(command, "commands", None)
+        settings = await self.get_member_settings(ctx.author.id)
+
+        description = _(
+            "Use `{prefix}settings [setting] [value]` to update a value "
+            "of a specific setting. E.g.: `{prefix}settings color blue` "
+            "will set the embeds color to blue.\n\n**Your settings**"
+        ).format(prefix=ctx.prefix)
+
+        embed = discord.Embed(color=self.bot.color(ctx.author.id))
+        author_name = _("{member}'s Settings").format(member=ctx.author.name)
+        embed.set_author(name=author_name, icon_url=ctx.author.avatar_url)
+        embed.description = description
+
+        for subcommand in subcommands:
+            if subcommand.short_doc:
+                # remove `[Premium]` from docstring
+                short_doc = subcommand.short_doc[12:]
+            else:
+                short_doc = _("No help found...")
+
+            name = subcommand.name.capitalize()
+            value = "*{description}*\nCurrent: `{setting}`".format(
+                description=short_doc, setting=settings[subcommand.name]
+            )
+            embed.add_field(name=name, value=value, inline=False)
+
+        return embed
+
     @is_premium()
     @commands.group(invoke_without_command=True)
     @locale
     async def settings(self, ctx, command: str = None):
         _("""`[Premium]` Update your settings.""")
-        embed = self.bot.get_subcommands(ctx, ctx.command)
+        embed = await self.embed_member_settings(ctx, ctx.command)
         await ctx.send(embed=embed)
 
     @is_premium()
@@ -62,40 +100,35 @@ class Member(commands.Cog):
         `<color>` - The color to use for the embeds.
 
         Formats:
-        - Either 3 or 6 hex digit: #RGB or #RRGGBB.
+        - Either 3 or 6 hex digit: #RGB or #RRGGBB
         - Color code: green, white, red etc...
 
         Enter `none` or `default` to reset the color.
         Notice that few embeds will not change their color.
         """
         )
-        # TODO: enter `none` or `default` to reset color
+
+        if color in ("none", "default"):
+            query = "UPDATE member SET embed_color = NULL WHERE id = $1;"
+            await self.bot.pool.execute(query, ctx.author.id)
+            del self.bot.embed_colors[ctx.author.id]
+            return await ctx.send(_("Color successfully reset."))
+
         try:
-            if not Color(color) == Color("white"):
-                c = Color(color).get_hex_l()
-            else:
-                # pure white doesn't work for embeds
-                c = Color("#fffff0").get_hex_l()
+            clr = Color(color).get_hex_l()
         except (AttributeError, ValueError):
-            embed = discord.Embed(color=discord.Color.red())
-            embed.description = _(
-                "Invalid color! Supported color formats:\n"
-                "1. Either 3 or 6 hex digit: `#RGB` or `#RRGGBB` \n"
-                "2. Color code. Example: `green`, `white`, `red`, etc...\n"
-                "[Click here](https://htmlcolorcodes.com/color-names/) to have a look at the available color codes."
+            message = _(
+                "You need to specify a hex (Example: `#00ff00`) or a color code (Example: `red`)."
             )
-            return await ctx.send(embed=embed)
+            return await ctx.send(message)
 
-        # since discord.Color() takes only a raw integer value,
-        # the color must be converted to a base 16 integer and
-        # then passed to discord.Color()
-        c = int(c.lstrip("#"), 16)
-        embed = discord.Embed(color=discord.Color(c))
+        clr = int(clr.lstrip("#"), 16)
+        embed = discord.Embed(color=clr)
 
         try:
-            query = "UPDATE member SET embed_color = $1 WHERE id = $1;"
-            await self.bot.pool.execute(query, c, ctx.author.id)
-            self.bot.embed_colors[ctx.author.id] = c
+            query = "UPDATE member SET embed_color = $1 WHERE id = $2;"
+            await self.bot.pool.execute(query, clr, ctx.author.id)
+            self.bot.embed_colors[ctx.author.id] = clr
         except Exception as e:
             await ctx.send(embed=self.bot.embed_exception(e))
         else:
