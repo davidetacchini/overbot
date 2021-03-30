7605de3ee462b1673174c3a47b1bccc5ae78ff95
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-03-30 22:08:47 +0200

    Remove reddit logo

--- config.example.py
+++ config.example.py
@@ -116,9 +116,6 @@ overwatch = {
 """Python logo."""
 python_logo = "https://i.imgur.com/6pg6Xv4.png"
 
-"""Reddit logo."""
-reddit_logo = "https://i.imgur.com/ZBwI6VN.png"
-
 """Official website."""
 website = "https://overbot.netlify.app"
 
