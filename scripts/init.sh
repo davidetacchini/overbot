fc0f5ad6d1b8a63b5f2d44fb686db3a454ed7343
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-06-05 17:40:45 +0200

    Check for the database to exists

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 red=$(tput setaf 1)
 green=$(tput setaf 2)
-reset=$(tput sgr0)
-
 bold=$(tput bold)
-normal=$(tput sgr0)
+reset=$(tput sgr0)
 
 if [ "$EUID" -ne 0 ]; then 
 	echo "${red}You must have root permission.${reset}"
@@ -13,20 +11,27 @@ else
  	echo "[${green}OK${reset}] root permissions"
 fi
 
-printf "Welcome to the OverBot Setup!\n\n"
+printf "${bold}Welcome to the OverBot Setup!${reset}\n\n"
 
-printf "Check for the config file to be installed..."
-
-if [ ! -f "./config.example.py" ]; then
-	printf "\n\nInstalling the configuration file...\n"
-	curl https://raw.githubusercontent.com/davidetacchini/OverBot/master/config.example.py
-	printf "[${green}OK${reset}] configuration file successfully installed.\n"
+printf "Checking for the database ${bold}overbot${reset} to exists...\n"
+if [ "$( psql overbot -tAc 'SELECT 1' 2>&1 )" = '1' ]; then
+    printf "[${green}OK${reset}] Database ${bold}overbot${reset} exists.\n"
+else
+    printf "${red}Database overbot doesn't exists. Please follow all the README instructions.${reset}\n"
+    exit 1
 fi
 
-printf "\n\Moving config.example.py to config.py...\n"
-mv config.example.py config.py
+printf "Checking for the configuration file to be installed..."
+if [ ! -f "./config.example.py" ]; then
+	printf "\nInstalling the configuration file...\n"
+	curl https://raw.githubusercontent.com/davidetacchini/overbot/master/config.example.py -o config.py
+	printf "[${green}OK${reset}] configuration file successfully installed.\n"
+else
+    printf "Renaming config.example.py to config.py...\n"
+    mv config.example.py config.py
+fi
 
-printf "Copying service file to /etc/systemd/system/overbot.service...\n\n"
+printf "Copying service file to /etc/systemd/system/overbot.service...\n"
 sed -i "s:/path/to/overbot/:$(pwd)/:" overbot.service
 sed -i "s:username:$(whoami):" overbot.service
 cp overbot.service /etc/systemd/system/overbot.service
@@ -36,10 +41,10 @@ sudo -u davide psql overbot < schema.sql
 
 printf "Reloading the daemon...\n"
 {
-	systemctl daemon-reload
+	sudo systemctl daemon-reload
 } || {
 	echo "[${red}ERROR${reset}] an error occured while reloading the daemon."
 }
 
-printf "${green}${bold}Installation completed!${normal}${reset}\n"
+printf "${green}${bold}Installation completed!${reset}${reset}\n"
 printf "${red}Checkout the README file to complete the installation.${reset}\n"
