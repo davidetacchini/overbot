644e37f065c706994c22958f01dffa6b3e568980
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-06-14 01:20:48 +0200

    update converters

--- classes/converters.py
+++ classes/converters.py
@@ -2,70 +2,17 @@ from discord.ext import commands
 
 from utils.i18n import _
 
-PC = ("pc", "bnet")
-XBOX = ("xbl", "xbox")
-PLAYSTATION = ("ps", "psn", "play", "playstation")
-NINTENDO_SWITCH = ("nsw", "switch", "nintendo-switch")
-PLATFORMS = XBOX + PLAYSTATION + NINTENDO_SWITCH + PC
 
-
-class InvalidPlatform(commands.BadArgument):
-    """Exception raised when an invalid platform is given."""
-
-    def __init__(self, ctx):
-        super().__init__(
-            _('Invalid platform. Use "{prefix}help {command}" for more info.').format(
-                prefix=ctx.prefix, command=ctx.command.qualified_name
-            )
-        )
-
-
-class InvalidHero(commands.BadArgument):
-    """Exception raised when an invalid hero is given."""
-
+class UnknownHero(commands.BadArgument):
     def __init__(self, hero):
-        super().__init__(_("Hero **{hero}** doesn't exist.").format(hero=hero))
-
-
-class InvalidMemeCategory(commands.BadArgument):
-    """Exception raised when an invalid meme category is given."""
-
-    def __init__(self, ctx):
-        super().__init__(
-            _('Invalid category. Use "{prefix}help {command}" for more info.').format(
-                prefix=ctx.prefix, command=ctx.command.qualified_name
-            )
-        )
-
-
-class InvalidIndex(commands.BadArgument):
-    """Exception raised when an invalid index is given."""
-
-    def __init__(self):
-        super().__init__(_("Index must be a number."))
-
-
-class Platform(commands.Converter):
-    async def convert(self, ctx, platform):
-        platform = platform.lower()
-        if platform not in PLATFORMS:
-            raise InvalidPlatform(ctx)
-        elif platform in PC:
-            return "pc"
-        elif platform in PLAYSTATION:
-            return "psn"
-        elif platform in XBOX:
-            return "xbl"
-        elif platform in NINTENDO_SWITCH:
-            return "nintendo-switch"
+        super().__init__(_("Unknown hero: **{hero}**.").format(hero=hero))
 
 
 class Hero(commands.Converter):
-    async def convert(self, ctx, hero):
-        hero = hero.lower()
+    async def convert(self, ctx, argument):
+        hero = argument.lower()
         if hero not in ctx.bot.hero_names:
-            raise InvalidHero(hero)
-        # Advanced A.I. implementation
+            raise UnknownHero(hero)
         elif hero in ("soldier", "soldier-76"):
             return "soldier76"
         elif hero == "wreckingball":
@@ -77,31 +24,7 @@ class Hero(commands.Converter):
         return hero
 
 
-class HeroCategory(commands.Converter):
-    async def convert(self, ctx, category):
-        category = category.lower()
-        if category in ("heal", "healer"):
-            return "support"
-        elif category == "dps":
-            return "damage"
-        return category
-
-
-class MapCategory(commands.Converter):
-    async def convert(self, ctx, category):
-        return category.lower()
-
-
-class MemeCategory(commands.Converter):
-    async def convert(self, ctx, category):
-        category = category.lower()
-        if category not in ("hot", "new", "top", "rising"):
-            raise InvalidMemeCategory(ctx)
-        return category
-
-
-class Index(commands.Converter):
-    async def convert(self, ctx, index):
-        if not index.isdigit():
-            raise InvalidIndex()
-        return int(index)
+def valid_index(argument):
+    if not argument.isdigit():
+        raise commands.BadArgument(_("Index must be a number."))
+    return int(argument)
