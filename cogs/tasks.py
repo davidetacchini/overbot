6c3e67b68cbf36466fb5ae7627c3aca9bb618e1b
davidetacchini <davidetacchini11@gmail.com>
2021-07-18 17:03:00 +0200

    handle KeyError in subscription task

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -222,30 +222,37 @@ class Tasks(commands.Cog):
         headers = {"Authorization": self.bot.config.dbot["api_key"]}
 
         async with self.bot.session.get(url_new, headers=headers) as r:
-            subs = await r.json()
+            subscriptions = await r.json()
 
-        if not subs["donations"]:
+        try:
+            donations = subscriptions["donations"]
+        except KeyError:
             return
 
-        for sub in subs["donations"]:
-            if sub["product_id"] == product_server_id:
+        if not donations:
+            return
+
+        for dono in donations:
+            if dono["product_id"] == product_server_id:
                 guild_id = int(
-                    sub["seller_customs"]["Server ID (to be set as premium)"]
+                    dono["seller_customs"]["Server ID (to be set as premium)"]
                 )
                 await self.set_premium_for(guild_id)
                 self.bot.premiums.add(guild_id)
             else:
-                member_id = int(sub["buyer_id"])
+                member_id = int(dono["buyer_id"])
                 await self.set_premium_for(member_id, server=False)
                 self.bot.premiums.add(member_id)
 
             # endpoint to mark donation as processed
-            url_mark = self.bot.config.dbot["mark"].format(sub["txn_id"])
+            url_mark = self.bot.config.dbot["mark"].format(dono["txn_id"])
             payload = {"markProcessed": True}
             async with self.bot.session.post(
                 url_mark, json=payload, headers=headers
             ) as r:
-                print(f'Donation {sub["txn_id"]} has been processed. Status {r.status}')
+                print(
+                    f'Donation {dono["txn_id"]} has been processed. Status {r.status}'
+                )
 
     @tasks.loop(minutes=5.0)
     async def send_overwatch_news(self):
