d2262dfe0ba7781108e7845406daaa8d7cd1662d
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-10-18 22:29:15 +0200

    emojis dedicated file

---
+++ utils/emojis.py
@@ -0,0 +1,15 @@
+from discord import PartialEmoji
+
+battlenet = PartialEmoji(name="battlenet", id=679469162724196387)
+psn = PartialEmoji(name="psn", id=679468542541693128)
+xbl = PartialEmoji(name="xbl", id=679469487623503930)
+switch = PartialEmoji(name="nsw", id=752653766377078817)
+
+# emojis for custom discord username (skill rating)
+u_tank = ("\N{SHIELD}",)
+u_damage = ("\N{CROSSED SWORDS}",)
+u_support = ("\N{HEAVY GREEK CROSS}",)
+
+online = PartialEmoji(name="online", id=648186001361076243)
+dnd = PartialEmoji(name="dnd", id=648185968209428490)
+offline = PartialEmoji(name="offline", id=648185992360099865)
