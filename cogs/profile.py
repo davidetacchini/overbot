1464e757ca3d4bc27a6102d1009bc5076749c716
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-10-19 16:16:55 +0200

    Revert "add assets"
    This reverts commit 73c6d7328fba70a26d5e82d2e10461f776b554ce.

--- cogs/profile.py
+++ cogs/profile.py
@@ -11,26 +11,25 @@ import matplotlib
 from matplotlib import pyplot
 from discord.ext import commands
 
-from utils import emojis
-from classes.ui import DropdownPlatform
+from utils.i18n import _, locale
 from utils.checks import is_premium, has_profile, can_add_profile
 from classes.player import Player
 from classes.request import Request
-from classes.paginator import Update
+from classes.paginator import Link, Update
 from classes.converters import Hero
 from classes.exceptions import NoChoice
 
 MAX_NICKNAME_LENGTH = 32
 ROLES = {
-    "tank": emojis.u_tank,
-    "damage": emojis.u_damage,
-    "support": emojis.u_support,
+    "tank": "\N{SHIELD}",
+    "damage": "\N{CROSSED SWORDS}",
+    "support": "\N{HEAVY GREEK CROSS}",
 }
 PLATFORMS = {
-    "pc": emojis.battlenet,
-    "psn": emojis.psn,
-    "xbl": emojis.xbl,
-    "nintendo-switch": emojis.switch,
+    "pc": "<:battlenet:679469162724196387>",
+    "psn": "<:psn:679468542541693128>",
+    "xbl": "<:xbl:679469487623503930>",
+    "nintendo-switch": "<:nsw:752653766377078817>",
 }
 
 
@@ -41,7 +40,7 @@ async def chunker(entries, chunk):
 
 def valid_index(argument):
     if not argument.isdigit():
-        raise commands.BadArgument("Index must be a number.")
+        raise commands.BadArgument(_("Index must be a number."))
     return int(argument)
 
 
@@ -71,7 +70,7 @@ class Profile(commands.Cog):
             try:
                 profile = profiles[abs(index) - 1]
             except IndexError:
-                raise commands.BadArgument("Invalid index.") from None
+                raise commands.BadArgument(_("Invalid index.")) from None
         else:
             query = """SELECT profile.id, platform, username
                        FROM profile
@@ -114,8 +113,12 @@ class Profile(commands.Cog):
 
         for chunk in chunks:
             embed = discord.Embed(color=self.bot.color(ctx.author.id))
-            embed.set_author(name=str(ctx.author), icon_url=ctx.author.display_avatar)
-            embed.set_footer(text=f"{len(profiles)}/{limit} profiles")
+            embed.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
+            embed.set_footer(
+                text=_("{profiles}/{limit} profiles").format(
+                    profiles=len(profiles), limit=limit
+                )
+            )
 
             description = []
             for (id_, platform, username) in chunk:
@@ -132,13 +135,13 @@ class Profile(commands.Cog):
 
     async def get_player_username(self, ctx, platform):
         if platform == "pc":
-            await ctx.send("Enter your BattleTag (format: name#0000):")
+            await ctx.send(_("Enter your BattleTag (format: name#0000):"))
         elif platform == "psn":
-            await ctx.send("Enter your Online ID:")
+            await ctx.send(_("Enter your Online ID:"))
         elif platform == "xbl":
-            await ctx.send("Enter your Gamertag:")
+            await ctx.send(_("Enter your Gamertag:"))
         elif platform == "nintendo-switch":
-            await ctx.send("Enter your Nintendo Switch ID:")
+            await ctx.send(_("Enter your Nintendo Switch ID:"))
         else:
             return
 
@@ -179,7 +182,9 @@ class Profile(commands.Cog):
         name = member.name[:x]
         return name + " " + tmp
 
-    async def set_or_remove_nickname(self, ctx, *, profile=None, profile_id=None, remove=False):
+    async def set_or_remove_nickname(
+        self, ctx, *, profile=None, profile_id=None, remove=False
+    ):
         member = ctx.author
 
         if not remove:
@@ -191,19 +196,23 @@ class Profile(commands.Cog):
             await member.edit(nick=nick)
         except discord.Forbidden:
             await ctx.send(
-                "I can't change nicknames in this server. Grant me `Manage Nicknames` permission."
+                _(
+                    "I can't change nicknames in this server. Grant me `Manage Nicknames` permission."
+                )
             )
         except discord.HTTPException:
-            await ctx.send("Something bad happened while updating your nickname.")
+            await ctx.send(_("Something bad happened while updating your nickname."))
 
         if not remove:
-            query = "INSERT INTO nickname(id, server_id, profile_id) VALUES($1, $2, $3);"
+            query = (
+                "INSERT INTO nickname(id, server_id, profile_id) VALUES($1, $2, $3);"
+            )
             await self.bot.pool.execute(query, member.id, ctx.guild.id, profile_id)
-            await ctx.send("Nickname successfully set.")
+            await ctx.send(_("Nickname successfully set."))
         else:
             query = "DELETE FROM nickname WHERE id = $1;"
             await self.bot.pool.execute(query, member.id)
-            await ctx.send("Nickname successfully removed.")
+            await ctx.send(_("Nickname successfully removed."))
 
     async def update_nickname_sr(self, member, *, profile):
         if not await self.has_nickname(member.id):
@@ -214,30 +223,30 @@ class Profile(commands.Cog):
             await member.edit(nick=nick)
 
     @has_profile()
-    @commands.group(invoke_without_command=True, brief="Lists your profiles.")
+    @commands.group(invoke_without_command=True, brief=_("Lists your profiles."))
+    @locale
     async def profile(self, ctx, member: discord.Member = None):
-        """List your profiles.
+        _(
+            """List your profiles.
 
         `[member]` - The mention or the ID of a Discord member.
 
         If no member is given, the profiles returned will be yours.
         """
+        )
         member = member or ctx.author
         profiles = await self.get_profiles(ctx, member=member)
         embed = await self.list_profiles(ctx, profiles)
         await self.bot.paginator.Paginator(pages=embed).start(ctx)
 
     @can_add_profile()
-    @profile.command(aliases=["add"], brief="Link an Overwatch profile.")
+    @profile.command(aliases=["add"], brief=_("Link an Overwatch profile."))
+    @locale
     async def link(self, ctx):
-        """Link an Overwatch profile to your Discord account."""
-        view = discord.ui.View()
-        view.add_item(DropdownPlatform())
-        await ctx.send("Link a profile", view=view)
-        await view.wait()
-
-        platform = view.children[0].platform
+        _("""Link an Overwatch profile to your Discord account.""")
+        platform = await Link().start(ctx)
         username = await self.get_player_username(ctx, platform)
+
         if not username:
             return
 
@@ -247,45 +256,57 @@ class Profile(commands.Cog):
             query = "SELECT id FROM profile WHERE member_id = $1;"
             profile_id = await self.bot.pool.fetchval(query, ctx.author.id)
             await self.set_main_profile(ctx.author.id, profile_id=profile_id)
-        await ctx.send("Profile successfully linked.")
+        await ctx.send(_("Profile successfully linked."))
 
     @has_profile()
-    @profile.command(aliases=["remove"], brief="Unlink a profile.")
+    @profile.command(aliases=["remove"], brief=_("Unlink a profile."))
+    @locale
     async def unlink(self, ctx, index: valid_index):
-        """Unlink a profile.
+        _(
+            """Unlink a profile.
 
         `<index>` - The profile's index to unlink.
 
         You can't unlink the main profile if multiple profiles are linked.
         """
+        )
         id_, platform, username = await self.get_profile(ctx, index=index)
         profiles = await self.get_profiles(ctx)
-        if await self.is_main_profile(ctx.author.id, profile_id=id_) and len(profiles) > 1:
-            message = (
+        if (
+            await self.is_main_profile(ctx.author.id, profile_id=id_)
+            and len(profiles) > 1
+        ):
+            message = _(
                 "You can't unlink the main profile if multiple profiles are linked. "
-                f'Use "{ctx.prefix}help profile main" for more info.'
-            )
+                'Use "{prefix}help profile main" for more info.'
+            ).format(prefix=ctx.prefix)
             return await ctx.send(message)
 
         if platform == "pc":
             username = username.replace("-", "#")
 
-        embed = discord.Embed(color=self.bot.color(ctx.author.id))
-        embed.title = "Are you sure you want to unlink the following profile?"
-        embed.add_field(name="Platform", value=platform)
-        embed.add_field(name="Username", value=username)
+        if not await ctx.prompt(
+            _(
+                "This will unlink the following profile:\n"
+                "Platform: `{platform}`\n"
+                "Username: `{username}`"
+            ).format(platform=platform, username=username)
+        ):
+            return
 
-        if await ctx.prompt(embed):
-            await self.bot.pool.execute("DELETE FROM profile WHERE id = $1;", id_)
-            await ctx.send("Profile successfully deleted.")
+        await self.bot.pool.execute("DELETE FROM profile WHERE id = $1;", id_)
+        await ctx.send(_("Profile successfully unlinked."))
 
     @has_profile()
-    @profile.command(brief="Update a profile.")
+    @profile.command(brief=_("Update a profile."))
+    @locale
     async def update(self, ctx, index: valid_index):
-        """Update a profile.
+        _(
+            """Update a profile.
 
         `<index>` - The profile's index to update.
         """
+        )
         id_, platform, username = await self.get_profile(ctx, index=index)
         platform = await Update(platform, username).start(ctx)
         username = await self.get_player_username(ctx, platform)
@@ -300,26 +321,35 @@ class Profile(commands.Cog):
         await ctx.send("Profile successfully updated.")
 
     @has_profile()
-    @profile.command(brief="Update the main profile.")
+    @profile.command(brief=_("Update the main profile."))
+    @locale
     async def main(self, ctx, index: valid_index):
-        """Update the main profile.
+        _(
+            """Update the main profile.
 
         `<index>` - The profile's index to set as main.
 
         Defaults to the first profile you have linked.
         """
+        )
         id_, platform, username = await self.get_profile(ctx, index=index)
         await self.set_main_profile(ctx.author.id, profile_id=id_)
         embed = discord.Embed(color=self.bot.color(ctx.author.id))
-        embed.description = "Main profile successfully set to:"
-        embed.add_field(name="Platform", value=platform)
-        embed.add_field(name="Username", value=username)
+        embed.description = _("Main profile successfully set to:")
+        embed.add_field(name=_("Platform"), value=platform)
+        embed.add_field(name=_("Username"), value=username)
         await ctx.send(embed=embed)
 
     @has_profile()
-    @profile.command(aliases=["rank", "sr"], brief="Provides SRs information for a profile.")
-    async def rating(self, ctx, index: valid_index = None, member: discord.Member = None):
-        """Provides SRs information for a profile.
+    @profile.command(
+        aliases=["rank", "sr"], brief=_("Provides SRs information for a profile.")
+    )
+    @locale
+    async def rating(
+        self, ctx, index: valid_index = None, member: discord.Member = None
+    ):
+        _(
+            """Provides SRs information for a profile.
 
         `[index]` - The profile's index to see the ratings for.
         `[member]` - The mention or the ID of a Discord member.
@@ -329,9 +359,12 @@ class Profile(commands.Cog):
 
         Provide both the index and the member to see a member's ratings.
         """
+        )
         async with ctx.typing():
             member = member or ctx.author
-            id_, platform, username = await self.get_profile(ctx, member=member, index=index)
+            id_, platform, username = await self.get_profile(
+                ctx, member=member, index=index
+            )
 
             data = await Request(platform, username).get()
             profile = Player(data, platform=platform, username=username)
@@ -345,9 +378,15 @@ class Profile(commands.Cog):
             await ctx.send(embed=embed)
 
     @has_profile()
-    @profile.command(aliases=["statistics"], brief="Provides general stats for a profile.")
-    async def stats(self, ctx, index: valid_index = None, member: discord.Member = None):
-        """Provides general stats for a profile.
+    @profile.command(
+        aliases=["statistics"], brief=_("Provides general stats for a profile.")
+    )
+    @locale
+    async def stats(
+        self, ctx, index: valid_index = None, member: discord.Member = None
+    ):
+        _(
+            """Provides general stats for a profile.
 
         `[index]` - The profile's index to see the stats for.
         `[member]` - The mention or the ID of a Discord member.
@@ -357,18 +396,25 @@ class Profile(commands.Cog):
 
         Provide both the index and the member to see a member's stats.
         """
+        )
         async with ctx.typing():
             member = member or ctx.author
-            unused, platform, username = await self.get_profile(ctx, member=member, index=index)
+            unused, platform, username = await self.get_profile(
+                ctx, member=member, index=index
+            )
 
             cog = self.bot.get_cog("Stats")
             if cog:
                 await cog.show_stats_for(ctx, "allHeroes", platform, username)
 
     @has_profile()
-    @profile.command(brief="Provides general hero stats for a profile.")
-    async def hero(self, ctx, hero: Hero, index: valid_index = None, member: discord.Member = None):
-        """Provides general hero stats for a profile.
+    @profile.command(brief=_("Provides general hero stats for a profile."))
+    @locale
+    async def hero(
+        self, ctx, hero: Hero, index: valid_index = None, member: discord.Member = None
+    ):
+        _(
+            """Provides general hero stats for a profile.
 
         `<hero>` - The name of the hero to see stats for.
         `[index]` - The profile's index to see the stats for.
@@ -379,34 +425,42 @@ class Profile(commands.Cog):
 
         Provide both the index and the member to see a member's stats.
         """
+        )
         async with ctx.typing():
             member = member or ctx.author
-            unused, platform, username = await self.get_profile(ctx, member=member, index=index)
+            unused, platform, username = await self.get_profile(
+                ctx, member=member, index=index
+            )
 
             cog = self.bot.get_cog("Stats")
             if cog:
                 await cog.show_stats_for(ctx, hero, platform, username)
 
     @has_profile()
-    @profile.command(aliases=["nick"], brief="Shows your SRs in your nickname.")
+    @profile.command(aliases=["nick"], brief=_("Shows your SRs in your nickname."))
     @commands.guild_only()
+    @locale
     async def nickname(self, ctx):
-        """Shows your SRs in your nickname.
+        _(
+            """Shows your SRs in your nickname.
 
         The nickname can only be set in one server.
 
         The nickname updates automatically whenever `profile rating`
         is used and the profile matches the one set for the nickname.
         """
+        )
         if not await self.has_nickname(ctx.author.id):
-            if not await ctx.prompt("This will display your SRs in your nickname."):
+            if not await ctx.prompt(_("This will display your SRs in your nickname.")):
                 return
 
             if ctx.guild.me.top_role < ctx.author.top_role:
                 return await ctx.send(
-                    "This server's owner needs to move the `OverBot` role higher, so I will "
-                    "be able to update your nickname. If you are this server's owner, there's "
-                    "not way for me to change your nickname, sorry!"
+                    _(
+                        "This server's owner needs to move the `OverBot` role higher, so I will "
+                        "be able to update your nickname. If you are this server's owner, there's "
+                        "not way for me to change your nickname, sorry!"
+                    )
                 )
 
             id_, platform, username = await self.get_profile(ctx)
@@ -421,7 +475,7 @@ class Profile(commands.Cog):
             except Exception as e:
                 await ctx.send(e)
         else:
-            if await ctx.prompt("This will remove your SR in your nickname."):
+            if await ctx.prompt(_("This will remove your SR in your nickname.")):
                 try:
                     await self.set_or_remove_nickname(ctx, remove=True)
                 except Exception as e:
@@ -453,7 +507,9 @@ class Profile(commands.Cog):
                 data.drop(row, axis=1, inplace=True)
 
         if len(data.columns) == 0:
-            raise commands.BadArgument("I don't have enough data to create the graph.")
+            raise commands.BadArgument(
+                _("I don't have enough data to create the graph.")
+            )
 
         fig, ax = pyplot.subplots()
         ax.xaxis_date()
@@ -475,20 +531,23 @@ class Profile(commands.Cog):
         file = discord.File(image, filename="graph.png")
 
         embed = discord.Embed(color=self.bot.color(ctx.author.id))
-        embed.set_author(name=str(ctx.author), icon_url=ctx.author.display_avatar)
+        embed.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
         embed.set_image(url="attachment://graph.png")
         return file, embed
 
     @is_premium()
     @has_profile()
-    @profile.command(brief="`[Premium]` Shows SRs performance graph.")
+    @profile.command(brief=_("`[Premium]` Shows SRs performance graph."))
+    @locale
     async def graph(self, ctx, index: valid_index = None):
-        """`[Premium]` Shows SRs performance graph.
+        _(
+            """`[Premium]` Shows SRs performance graph.
 
         `[index]` - The profile's index to see the SR graph for.
 
         If no index is given, the profile used will be the main one.
         """
+        )
         profile = await self.get_profile(ctx, index=index)
         file, embed = await self.sr_graph(ctx, profile=profile)
         await ctx.send(file=file, embed=embed)
