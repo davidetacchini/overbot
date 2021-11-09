8b155ad7c353ea2960b6ce0cbb40f68c9bcb59fc
Davide <84925446+davidetacchini@users.noreply.github.com>
2021-11-09 21:42:53 +0100

    create a funcs.py file for general functions

---
+++ utils/funcs.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from discord import Embed, PartialEmoji
+
+from . import emojis
+
+
+def get_platform_emoji(platform: str) -> PartialEmoji:
+    lookup = {
+        "pc": emojis.battlenet,
+        "psn": emojis.psn,
+        "xbl": emojis.xbl,
+        "nintendo-switch": emojis.switch,
+    }
+    return lookup[platform]
+
+
+async def chunker(pages: Union[str, dict, Embed], *, per_page: int):
+    for x in range(0, len(pages), per_page):
+        yield pages[x : x + per_page]
