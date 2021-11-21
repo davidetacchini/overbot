5a8adb3301f28f0a2fd57fbd7ba58ef364c78759
davidetacchini <davidetacchini11@gmail.com>
2021-11-21 15:55:47 +0100

    add PaginationError custom exception

--- classes/exceptions.py
+++ classes/exceptions.py
@@ -1,3 +1,12 @@
 class NoChoice(Exception):
     def __init__(self):
         super().__init__("Took too long.")
+
+
+class PaginationError(Exception):
+    pass
+
+
+class CannotEmbedLinks(PaginationError):
+    def __init__(self):
+        super().__init__("Bot cannot embed links in this channel.")
