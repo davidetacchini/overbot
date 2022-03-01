20a9a0f18b6d8cab9c077fa457ae33b766540b79
Davide <84925446+davidetacchini@users.noreply.github.com>
2022-03-01 22:05:05 +0100

    handling exceptions in compute_data mainly for error 443

--- classes/profile.py
+++ classes/profile.py
@@ -9,7 +9,7 @@ from asyncpg import Record
 from utils import emojis
 
 from .context import Context
-from .request import Request
+from .request import Request, UnexpectedError
 
 ROLES = {
     "tank": emojis.tank,
@@ -62,7 +62,10 @@ class Profile:
         self.pages = []
 
     async def compute_data(self):
-        self.data = await Request(self.platform, self.username).get()
+        try:
+            self.data = await Request(self.platform, self.username).get()
+        except Exception:
+            raise UnexpectedError() from None
 
     def __str__(self):
         return self.data["name"]
