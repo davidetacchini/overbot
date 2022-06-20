d47ac3fef2524c508893687b594cb4ecb4a23912
Davide <84925446+davidetacchini@users.noreply.github.com>
2022-06-20 23:18:34 +0200

    add InvalidColor exception; add type hints

--- classes/exceptions.py
+++ classes/exceptions.py
@@ -1,5 +1,8 @@
+from discord.app_commands import AppCommandError
+
+
 class NoChoice(Exception):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("Took too long.")
 
 
@@ -8,5 +11,12 @@ class PaginationError(Exception):
 
 
 class CannotEmbedLinks(PaginationError):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("Bot cannot embed links in this channel.")
+
+
+class InvalidColor(AppCommandError):
+    def __init__(self) -> None:
+        super().__init__(
+            "You must enter an hex value (e.g. `#fff`) or an rgb value *comma separated* (e.g. 255, 255, 255)."
+        )
