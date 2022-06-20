1cc1f848b59412b62d2b64d72fcc34f45f19e9b4
Davide <84925446+davidetacchini@users.noreply.github.com>
2022-06-20 23:26:25 +0200

    add autocomplete functions for heroes and modules

--- utils/funcs.py
+++ utils/funcs.py
@@ -1,6 +1,13 @@
-from discord import Embed, PartialEmoji
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord.app_commands import Choice
 
 from . import emojis
+
+if TYPE_CHECKING:
+    from discord import Embed, Interaction, PartialEmoji
 
 
 def get_platform_emoji(platform: str) -> PartialEmoji:
@@ -16,3 +23,27 @@ def get_platform_emoji(platform: str) -> PartialEmoji:
 async def chunker(pages: str | dict | Embed, *, per_page: int):
     for x in range(0, len(pages), per_page):
         yield pages[x : x + per_page]
+
+
+async def hero_autocomplete(interaction: Interaction, current: str):
+    # Just slice the list to return the first 25 heroes since the
+    # limit of displayable choices is 25 whereas the heroes are 32.
+    heroes = interaction.client.heroes
+    # The api uses different names for these heroes.
+    values = {
+        "soldier-76": "soldier76",
+        "wrecking-ball": "wreckingBall",
+        "dva": "dVa",
+    }
+    return [
+        Choice(name=hero, value=values.get(hero, hero))
+        for hero in heroes
+        if current.lower() in hero.lower()
+    ][:25]
+
+
+async def module_autocomplete(interaction: Interaction, current: str):
+    modules = interaction.client.extensions
+    return [
+        Choice(name=module, value=module) for module in modules if current.lower() in module.lower()
+    ]
