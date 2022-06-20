54eca403adb7d6e3c6faa7629126b984af156439
davidetacchini <davidetacchini11@gmail.com>
2022-06-20 23:25:24 +0200

    adapted for app_commands; add type hints

--- utils/checks.py
+++ utils/checks.py
@@ -1,88 +1,75 @@
-from discord.ext import commands
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import discord
+
+from discord import app_commands
+
+if TYPE_CHECKING:
+    from asyncpg import Record
 
 
-class ProfileNotLinked(commands.CheckFailure):
+class ProfileNotLinked(app_commands.CheckFailure):
 
     pass
 
 
-class ProfileLimitReached(commands.CheckFailure):
+class ProfileLimitReached(app_commands.CheckFailure):
     def __init__(self, limit):
         self.limit = limit
 
 
-class MemberNotPremium(commands.CheckFailure):
+class MemberNotPremium(app_commands.CheckFailure):
 
     pass
 
 
-class MemberOnCooldown(commands.CommandOnCooldown):
-
-    pass
-
-
-async def global_cooldown(ctx):
-    member_id = ctx.author.id
-    guild_id = ctx.guild.id if ctx.guild is not None else 0
-
-    if not ctx.bot.member_is_premium(member_id, guild_id):
-        bucket = ctx.bot.normal_cooldown.get_bucket(ctx.message)
-    else:
-        bucket = ctx.bot.premium_cooldown.get_bucket(ctx.message)
-
-    retry_after = bucket.update_rate_limit()
-    type_ = commands.BucketType.member
-
-    if retry_after:
-        raise MemberOnCooldown(bucket, retry_after, type_)
-    return True
-
-
-async def get_profiles(ctx):
+async def get_profiles(interaction: discord.Interaction) -> list[Record]:
     query = """SELECT platform, username
                FROM profile
                INNER JOIN member
                        ON member.id = profile.member_id
                WHERE member.id = $1;
             """
-    return await ctx.bot.pool.fetch(query, ctx.author.id)
+    return await interaction.client.pool.fetch(query, interaction.user.id)
 
 
 def has_profile():
     """Check for a user to have linked atleast a profile."""
 
-    async def predicate(ctx):
-        if await get_profiles(ctx):
+    async def predicate(interaction: discord.Interaction):
+        if await get_profiles(interaction):
             return True
         raise ProfileNotLinked()
 
-    return commands.check(predicate)
+    return app_commands.check(predicate)
 
 
 def can_add_profile():
     """Check for a user to have no profiles linked."""
 
-    async def predicate(ctx):
-        profiles = await get_profiles(ctx)
-        limit = ctx.bot.get_profiles_limit(ctx, ctx.author.id)
+    async def predicate(interaction: discord.Interaction):
+        profiles = await get_profiles(interaction)
+        limit = interaction.client.get_profiles_limit(interaction, interaction.user.id)
 
         if len(profiles) >= limit:
             raise ProfileLimitReached(limit)
         return True
 
-    return commands.check(predicate)
+    return app_commands.check(predicate)
 
 
 def is_premium():
     """Check for a user/server to be premium."""
 
-    async def predicate(ctx):
-        member_id = ctx.author.id
-        guild_id = ctx.guild.id if ctx.guild is not None else 0
+    async def predicate(interaction: discord.Interaction):
+        member_id = interaction.user.id
+        guild_id = interaction.guild.id if interaction.guild is not None else 0
         to_check = (member_id, guild_id)
 
-        if all(x not in ctx.bot.premiums for x in to_check):
+        if all(x not in interaction.client.premiums for x in to_check):
             raise MemberNotPremium()
         return True
 
-    return commands.check(predicate)
+    return app_commands.check(predicate)
