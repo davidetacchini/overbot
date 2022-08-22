aab02f9b008878dae98ab2e63547c704fbfc3651
Davide <84925446+davidetacchini@users.noreply.github.com>
2022-08-22 14:35:29 +0200

    add format_platform() and profile_autocomplete()

--- utils/funcs.py
+++ utils/funcs.py
@@ -10,12 +10,30 @@ if TYPE_CHECKING:
     from discord import Interaction, PartialEmoji
 
 
+platform_choices = [
+    Choice(name="Battle.net", value="pc"),
+    Choice(name="PlayStation", value="psn"),
+    Choice(name="XBOX", value="xbl"),
+    Choice(name="Nintendo Switch", value="nintendo-switch"),
+]
+
+
 def get_platform_emoji(platform: str) -> PartialEmoji:
     lookup = {
         "pc": emojis.battlenet,
         "psn": emojis.psn,
         "xbl": emojis.xbl,
         "nintendo-switch": emojis.switch,
+    }
+    return lookup[platform]
+
+
+def format_platform(platform: str) -> str:
+    lookup = {
+        "pc": "Battle.net",
+        "psn": "PlayStation",
+        "xbl": "XBOX",
+        "nintendo-switch": "Nintendo Switch",
     }
     return lookup[platform]
 
@@ -41,4 +59,14 @@ async def module_autocomplete(interaction: Interaction, current: str) -> list[Ch
     modules = interaction.client.extensions
     return [
         Choice(name=module, value=module) for module in modules if current.lower() in module.lower()
+    ]
+
+
+async def profile_autocomplete(interaction: Interaction, current: str) -> list[Choice[str]]:
+    profile_cog = interaction.client.get_cog("Profile")
+    profiles = await profile_cog.get_profiles(interaction, interaction.user.id)
+    return [
+        Choice(name=f"{format_platform(profile.platform)} - {profile.username}", value=profile.id)
+        for profile in profiles
+        if current.lower() in profile.username.lower()
     ]
