ef62935f67cbf3927eb9802fae9507fed373fe49
Davide <84925446+davidetacchini@users.noreply.github.com>
2022-12-20 19:22:20 +0100

    remove old overwatch rating icons

--- utils/emojis.py
+++ utils/emojis.py
@@ -14,16 +14,6 @@ tank = PartialEmoji(name="tank", id=645784573141319722)
 damage = PartialEmoji(name="damage", id=645784543093325824)
 support = PartialEmoji(name="support", id=645784563322191902)
 
-sr = PartialEmoji(name="sr", id=639897739920146437)
-
 online = PartialEmoji(name="online", id=648186001361076243)
 dnd = PartialEmoji(name="dnd", id=648185968209428490)
 offline = PartialEmoji(name="offline", id=648185992360099865)
-
-bronze = PartialEmoji(name="bronze", id=632281015863214096)
-silver = PartialEmoji(name="silver", id=632281054211997718)
-gold = PartialEmoji(name="gold", id=632281064596832278)
-platinum = PartialEmoji(name="platinum", id=632281092875091998)
-diamond = PartialEmoji(name="diamon", id=632281105571119105)
-master = PartialEmoji(name="master", id=632281117394993163)
-grand_master = PartialEmoji(name="grandmaster", id=632281128966946826)
