5118b7eb46b771e0703e3618cddadb3f0ab7c2f2
Davide <84925446+davidetacchini@users.noreply.github.com>
2023-07-26 23:42:56 +0200

    refactor get_overwatch_news(); using lxml as parser

--- utils/scrape.py
+++ utils/scrape.py
@@ -16,20 +16,21 @@ async def get_overwatch_news(amount: int) -> list[dict[str, str]]:
         news_id = fp.readline()
 
     content = await fetch(config.overwatch["news"] + str(news_id))
-    page = BeautifulSoup(content, features="html.parser")
 
-    news = page.find_all("a", {"class": "media@lg-min"})
-    if news is None:
-        raise Exception()
+    root_kwargs = {"name": "div", "class_": "main-content", "recursive": False}
+    root = BeautifulSoup(content, features="lxml").body.find(**root_kwargs)
 
-    news = list(news)[:amount]
+    news_container = root.find("div", class_="news-header", recursive=False).find(
+        "blz-news", recursive=False
+    )
 
-    all_news = []
-    for n in news:
-        cur_news = {}
-        cur_news["title"] = n.find("h3", class_="blog-sidebar-article-title").get_text()
-        cur_news["link"] = "https://overwatch.blizzard.com" + n["href"]
-        cur_news["thumbnail"] = "https:" + n.find("img", class_="media-card-fill")["src"]
-        cur_news["date"] = n.find("p", class_="blog-sidebar-date").get_text()
-        all_news.append(cur_news)
-    return all_news
+    news = [
+        {
+            "title": n.find("h4", slot="heading").get_text(),
+            "link": n["href"],
+            "thumbnail": "https:" + n.find("blz-image", slot="image")["src"],
+            "date": n["date"].split(":")[0][:-3],  # from YYYY-MM-DDT18:00:00.000Z to YYYY-MM-DD
+        }
+        for n in news_container.find_all("blz-card")
+    ]
+    return news
