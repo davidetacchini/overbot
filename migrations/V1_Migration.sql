969e5e77c4e43d60c907fc6a18d1a11844ab0e1c
Davide <84925446+davidetacchini@users.noreply.github.com>
2023-11-09 08:45:31 +0100

    format

---
+++ migrations/V1_Migration.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS command (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    guild_id bigint NOT NULL,
+    channel_id bigint NOT NULL,
+    author_id bigint NOT NULL,
+    created_at timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS member (
+    id bigint PRIMARY KEY NOT NULL,
+    embed_color integer,
+    premium boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS newsboard (
+    id bigint PRIMARY KEY NOT NULL,
+    server_id bigint REFERENCES server(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS profile (
+    id SERIAL PRIMARY KEY,
+    battletag varchar(100) NOT NULL,
+    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rating (
+    id SERIAL PRIMARY KEY,
+    tank smallint,
+    damage smallint,
+    support smallint,
+    date date DEFAULT CURRENT_DATE,
+    profile_id integer REFERENCES profile(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS server (
+    id bigint PRIMARY KEY NOT NULL,
+    premium boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS trivia (
+    id bigint PRIMARY KEY NOT NULL,
+    won integer DEFAULT 0 NOT NULL,
+    lost integer DEFAULT 0 NOT NULL,
+    started integer DEFAULT 0 NOT NULL
+);
---
+++ migrations/V2_Migration.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS news (
+    id integer DEFAULT 1 PRIMARY KEY NOT NULL,
+    latest_id integer DEFAULT 0 NOT NULL
+);
