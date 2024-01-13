dc4dae65bbb32602649f91b9c044a1daffb87360
Davide <84925446+davidetacchini@users.noreply.github.com>
2024-01-13 19:52:01 +0100

    fix migrations

--- migrations/V1_Migration.sql
+++ migrations/V1_Migration.sql
@@ -4,34 +4,34 @@
 
 CREATE TABLE IF NOT EXISTS command (
     id SERIAL PRIMARY KEY,
-    name text NOT NULL,
-    guild_id bigint NOT NULL,
-    channel_id bigint NOT NULL,
-    author_id bigint NOT NULL,
-    created_at timestamp NOT NULL
+    name text,
+    guild_id bigint,
+    channel_id bigint,
+    author_id bigint,
+    created_at timestamp
 );
 
 CREATE TABLE IF NOT EXISTS member (
-    id bigint PRIMARY KEY NOT NULL,
+    id bigint PRIMARY KEY,
     embed_color integer,
-    premium boolean DEFAULT false NOT NULL
+    premium boolean DEFAULT false
 );
 
 CREATE TABLE IF NOT EXISTS server (
-    id bigint PRIMARY KEY NOT NULL,
-    premium boolean DEFAULT false NOT NULL
+    id bigint PRIMARY KEY,
+    premium boolean DEFAULT false
 );
 
 CREATE TABLE IF NOT EXISTS newsboard (
-    id bigint PRIMARY KEY NOT NULL,
-    server_id bigint REFERENCES server(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
-    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+    id bigint PRIMARY KEY,
+    server_id bigint REFERENCES server(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS profile (
     id SERIAL PRIMARY KEY,
-    battletag varchar(100) NOT NULL,
-    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+    battletag varchar(100),
+    member_id bigint REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS rating (
@@ -40,12 +40,12 @@ CREATE TABLE IF NOT EXISTS rating (
     damage smallint,
     support smallint,
     date date DEFAULT CURRENT_DATE,
-    profile_id integer REFERENCES profile(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
+    profile_id integer REFERENCES profile(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS trivia (
-    id bigint PRIMARY KEY NOT NULL,
-    won integer DEFAULT 0 NOT NULL,
-    lost integer DEFAULT 0 NOT NULL,
-    started integer DEFAULT 0 NOT NULL
+    id bigint PRIMARY KEY,
+    won integer DEFAULT 0,
+    lost integer DEFAULT 0,
+    started integer DEFAULT 0
 );
--- migrations/V2_Migration.sql
+++ migrations/V2_Migration.sql
@@ -3,6 +3,6 @@
 -- Reason: Store latest overwatch news ID
 
 CREATE TABLE IF NOT EXISTS news (
-    id integer DEFAULT 1 PRIMARY KEY NOT NULL,
-    latest_id integer DEFAULT 0 NOT NULL
+    id integer DEFAULT 1 PRIMARY KEY,
+    latest_id integer DEFAULT 0
 );
